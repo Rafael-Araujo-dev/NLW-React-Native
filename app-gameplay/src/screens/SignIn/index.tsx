e14d713c767ccdd5918f18d6d19b7ac8dc7ddc8e
Rafael-Araujo-dev <web.dev@orafaelribeiro.com>
2021-10-21 16:11:27 -0300

    feat: adiciona interface signin

--- app-gameplay/src/screens/SignIn/index.tsx
+++ app-gameplay/src/screens/SignIn/index.tsx
@@ -1,29 +1,51 @@
-import React, { useState } from 'react'
-import { View, Text, TextInput } from 'react-native'
+import React from 'react'
+import { 
+            View, 
+            Text, 
+            Image,
+            StatusBar,
+            Button
+        } from 'react-native'
+import { string } from 'yargs'
+
+import IllustrationImg from '../../assets/illustration.png'
+
+import ButtonIcon from  '../../components/ButtonIcon'
 
 import  styles  from './styles'
 
 const SignIn = () => {
 
-    const [text, setText] = useState('');
-
-    let handleTextInput = (value: string) => {
-        setText(value);
-    }
-
     return (
         <View style= { styles.container }>
-            <Text>Hello World, NLW Togheter!</Text>
-
-            <Text style= { styles.youTyped }>
-                Você digitou: { text }
-            </Text>
-
-            <TextInput 
-                style= { styles.input }
-                placeholder= 'Digite aqui'
-                onChangeText= {handleTextInput}
+            <StatusBar 
+                barStyle= "light-content"
+                backgroundColor= "transparent"
+                translucent= { true }
             />
+            <Image 
+            source= { IllustrationImg } 
+            style= { styles.image }
+            resizeMode= "stretch"
+            />
+
+            <View style= { styles.content }>
+                <Text style= { styles.title }>
+                    Organize {`\n`}
+                    suas jogatinas {`\n`}
+                    facilmente
+                </Text>
+
+                <Text style= { styles.subtitle }>
+                    Crie grupos para jogar seus games {`\n`}
+                    favoritos com seus amigos
+                </Text>
+
+                <ButtonIcon 
+                    title= 'Entrar com Discord'
+                    activeOpacity= {.7}    
+                />
+            </View>
         </View>
     )
 }
