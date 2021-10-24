9e95586eeae19a0d603180d0fe2f3a305b4c55f9
Rafael-Araujo-dev <web.dev@orafaelribeiro.com>
2021-10-24 12:52:33 -0300

    feat: cria home screen

---
+++ app-gameplay/src/screens/Home/index.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+import styled from 'styled-components/native'
+
+const Container = styled.View`
+    flex: 1;
+`
+
+const Home = () => {
+    return (
+        <Container>
+            
+        </Container>
+    )
+}
+
+export default Home;
