7e74bc660424ead891aa2e9b929e0df651463a7d
Rafael-Araujo-dev <web.dev@orafaelribeiro.com>
2021-10-24 13:54:01 -0300

    feat: adiciona header a home screen

--- app-gameplay/src/screens/Home/index.tsx
+++ app-gameplay/src/screens/Home/index.tsx
@@ -1,15 +1,32 @@
 import React from 'react'
 
+//import { getStatusBarHeight } from 'react-native-iphone-x-helper'
+
 import styled from 'styled-components/native'
+
+import theme from '../../global/styles/theme'
+
+import Profile from '../../components/Profile'
 
 const Container = styled.View`
     flex: 1;
 `
 
+const Header = styled.View`
+    width: 100%;
+    padding: 0 24px;
+    flex-direction: row;
+    justify-content: space-between;
+    margin-top: /* calc( $/*{getStatusBarHeight()} + */ 46px;
+    margin-bottom: 42px;
+`
+
 const Home = () => {
     return (
         <Container>
-            
+            <Header>
+                <Profile />
+            </Header>
         </Container>
     )
 }
